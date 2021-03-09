03826250e79d2c957942c20716ba4f2017a73d45
bouzaaim <fa_bouzara@esi.dz>
2021-03-09 22:55:50 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let result = document.getElementById("result");
+let form = document.getElementById("form");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const numeral = document.getElementById("numeral").value;
+  result.innerHTML = convertNumberToWords(numeral);
+  result.style.visibility = "visible";
+};
+form.addEventListener("submit", handleSubmit);
+
+var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(
+  " "
+);
+var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
+function convertNumberToWords(n) {
+  if (n < 20) return num[n];
+  var digit = n % 10;
+  if (n < 100) return tens[~~(n / 10) - 2] + (digit ? "-" + num[digit] : "");
+  if (n < 1000)
+    return (
+      num[~~(n / 100)] +
+      " hundred" +
+      (n % 100 == 0 ? "" : " " + convertNumberToWords(n % 100))
+    );
+  return (
+    convertNumberToWords(~~(n / 1000)) +
+    " thousand" +
+    (n % 1000 != 0 ? " " + convertNumberToWords(n % 1000) : "")
+  );
+}
